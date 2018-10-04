d876b59ba3cd924a7ca1e7eb4801b832a4152fd2
zlcmo <zlcmo@10.12.16.102>
2018-10-04 17:44:46 +0100

    added array list

--- src/ie/gmit/studentmanager/StudentManager.java
+++ src/ie/gmit/studentmanager/StudentManager.java
@@ -1,12 +1,18 @@
 package ie.gmit.studentmanager;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StudentManager {
 	 //set initial capacity constant for array
 	private static final int INITIAL_CAPACITY = 10;
 	
 	//array to hold student objects
 	private Student[] students = null;
-	
+	//create array list called studentsAL
+	List<Student> studentsAL=new ArrayList<Student>();
+
 	//constructor
 	public StudentManager() {
 		students = new Student[INITIAL_CAPACITY];
@@ -28,4 +34,7 @@ public class StudentManager {
 	public int findTotalStudents() {
 		return -1;
 	}
+	
+	//convert student array to list
+	//List<Student> ArrayListStudents = new ArrayListStudents<Student>(Arrays.asList(students));
 }
