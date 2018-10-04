65cd5000052e6df44a87bb457704a1d0e0840a5d
zlcmo <zlcmo@10.12.16.102>
2018-10-04 17:54:38 +0100

    StudentsAL and add function

--- src/ie/gmit/studentmanager/StudentManager.java
+++ src/ie/gmit/studentmanager/StudentManager.java
@@ -6,26 +6,34 @@ import java.util.List;
 
 public class StudentManager {
 	 //set initial capacity constant for array
-	private static final int INITIAL_CAPACITY = 10;
+	//private static final int INITIAL_CAPACITY = 10;
 	
 	//array to hold student objects
-	private Student[] students = null;
+	//private Student[] students = null;
 	//create array list called studentsAL
-	List<Student> studentsAL=new ArrayList<Student>();
+	List<Student> studentsAL;
 
 	//constructor
 	public StudentManager() {
-		students = new Student[INITIAL_CAPACITY];
+		studentsAL = new ArrayList<Student>();
 	}
 	
 	//create methods may need & stub them out
 	public boolean add(Student student) {
+		return studentsAL.add(student);
+	}
+	
+	public boolean delete(String sid) {
+		//Student studentAL = getStudentByID(sid);
+		//if(student != null)
 		return false;
 	}
 	
-	public boolean delete(String studentId) {
-		return false;
-	}
+	//public Student getStudentByID(String sid) {
+		//for(Student student: studentsAL) {
+			
+		//}
+	//}
 	
 	public Student[] getStudentByFirstName(String firstName) {
 		return null;
@@ -35,6 +43,4 @@ public class StudentManager {
 		return -1;
 	}
 	
-	//convert student array to list
-	//List<Student> ArrayListStudents = new ArrayListStudents<Student>(Arrays.asList(students));
 }
